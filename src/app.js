ebe4dad18dbf0a6f6e52911f29f4a04612c4f809
InTheScencia <felipebarroy@gmail.com>
2024-11-10 22:40:19 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var who = [
+    "The dog ",
+    "My grandma ",
+    "The mailman ",
+    "My bird ",
+    "My Charmander "
+  ];
+
+  var action = ["ate ", "peed ", "crushed ", "broke ", "burned "];
+
+  var what = ["my homework ", "my phone ", "the car ", "my house "];
+
+  var when = [
+    "before the class ",
+    "when I was sleeping ",
+    "while I was exercising ",
+    "during my lunch ",
+    "while I was praying ",
+    "while I was studying"
+  ];
+
+  var exclamation = ["!!!!"];
+
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4] + exclamation;
 };
